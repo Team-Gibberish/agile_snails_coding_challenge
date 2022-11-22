8797fc95b7b3a9a44f73d141607e31545be19193
Julian Lenz <j.j.lenz@swansea.ac.uk>
2022-11-22 18:08:33 +0000

    Implemented time-dependent weather mocking

--- agile_snails_interface.py
+++ agile_snails_interface.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import json
+from typing import Dict, Any
 
 import pandas as pd
 import requests
@@ -16,7 +17,10 @@ CODE_TO_FILENAME = {
     "B1620": "dummy_data/generation.csv",
     "B0620": "dummy_data/load.csv",
 }
-METOFFICE_FILENAME = {"default": "dummy_data/met_office.json"}
+METOFFICE_TEMPLATE = "dummy_data/met_office_template.json"
+METOFFICE_DATAFILE = "dummy_data/weather_mock.csv"
+LATTITUDE = 52.1051
+LONGITUDE = -3.6680
 
 
 def mock_elexon_get_bmrs_report(code: str, date: str, period: int) -> pd.DataFrame:
@@ -28,18 +32,66 @@ def mock_elexon_get_bmrs_report(code: str, date: str, period: int) -> pd.DataFra
     )
 
 
-class MockResponse:
-    def __init__(self, json_content: str) -> None:
-        self._json_str = json_content
+def recursively_formatted(template_obj: Any, **kwargs: Any) -> Any:
+    if isinstance(template_obj, str):
+        return template_obj.format(**kwargs)
+    if isinstance(template_obj, dict):
+        return {
+            key: recursively_formatted(value, **kwargs)
+            for key, value in template_obj.items()
+        }
+    return [recursively_formatted(value, **kwargs) for value in template_obj]
 
-    def json(self) -> None:
-        return json.loads(self._json_str)
+
+class MockResponse:
+    def __init__(self, date: dt.date) -> None:
+        self._date = date
+        with open(METOFFICE_TEMPLATE, "r", encoding="utf-8") as file:
+            self._json_template = json.loads(file.read())
+        self._data = self._read_mock_data(date)
+
+    def _read_mock_data(self, date: dt.date) -> pd.DataFrame:
+        return (
+            pd.read_csv(
+                METOFFICE_DATAFILE,
+                header=0,
+                infer_datetime_format=True,
+                parse_dates=True,
+                index_col=["time", "lat", "lon"],
+                usecols=[
+                    "time",
+                    "lat",
+                    "lon",
+                    "screenTemperature",
+                    "windSpeed10m",
+                    "significantWeatherCode",
+                ],
+            )
+            .loc(axis=0)[date : date + dt.timedelta(days=2), LATTITUDE, LONGITUDE]
+            .droplevel(("lat", "lon"))
+            .resample("3h")
+            .mean()
+        )
+
+    def json(self) -> Dict[str, Any]:
+        return recursively_formatted(
+            self._json_template,
+            date=dt.datetime.combine(self._date, dt.time(hour=20)).strftime(
+                "%Y-%m-%dT%H:%M:%SZ"
+            ),
+            date_plus_one=dt.datetime.combine(
+                self._date + dt.timedelta(days=1), dt.time(hour=20)
+            ).strftime("%Y-%m-%dT%H:%M:%SZ"),
+            lat=LATTITUDE,
+            lon=LONGITUDE,
+            windspeed=self._data["windSpeed10m"],
+            temperature=self._data["screenTemperature"],
+            weathercode=self._data["significantWeatherCode"].astype(int),
+        )
 
 
 def mock_met_office_fetch_forecast(date: dt.date) -> MockResponse:
-    METOFFICE_FILENAME.setdefault(date, METOFFICE_FILENAME["default"])
-    with open(METOFFICE_FILENAME[date], "r", encoding="utf-8") as file:
-        return MockResponse(file.read())
+    return MockResponse(date)
 
 
 def mock_mongo_insert_one(*args, **kwargs) -> None:
